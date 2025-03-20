10046700424ed8f181a89f3be460fe95f363acaf
unknown <2992144@gmail.com>
2025-03-20 13:20:41 +0100

    Add .html .js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+var computerMove, playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
